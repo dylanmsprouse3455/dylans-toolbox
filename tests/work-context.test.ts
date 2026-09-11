7740197e5c7acdcbe97bc246ef4f072b775ee013
dylanmsprouse3455 <dylanm.sprouse3455@gmail.com>
2026-09-11 13:13:34 -0400

    Test filler-word case normalization

--- tests/work-context.test.ts
+++ tests/work-context.test.ts
@@ -7,6 +7,7 @@ test('normalizes common spoken case-number forms',()=>{
   assert.equal(normalizeWorkCaseNumbers('Check g260441 today.'),'Check G26-0441 today.');
   assert.equal(normalizeWorkCaseNumbers('Check G 20 6 0441 today.'),'Check G26-0441 today.');
   assert.equal(normalizeWorkCaseNumbers('Check G 2 6 0 4 4 1 today.'),'Check G26-0441 today.');
+  assert.equal(normalizeWorkCaseNumbers('G 26 is 0481 1445 Old Jonesboro Rd.'),'G26-0481 1445 Old Jonesboro Rd.');
 });
 
 test('work state marker is machine-readable but hidden from display content',()=>{
